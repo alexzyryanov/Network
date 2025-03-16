2f066cb7fe023862ffb93df5dd4c113638bc0e08
alexzyryanov <82859955+alexzyryanov@users.noreply.github.com>
2025-03-16 21:40:05 +0600

    init

--- Tests/NetworkTests/NetworkTests.swift
+++ Tests/NetworkTests/NetworkTests.swift
@@ -1,15 +1,4 @@
 import XCTest
 @testable import Network
 
-final class NetworkTests: XCTestCase {
-    
-    func testGoogle() async {
-        do {
-            let result = try await Network.fetchGoogle()
-            XCTAssertFalse(result.isEmpty)
-            
-        } catch {
-            XCTFail()
-        }
-    }
-}
+final class NetworkTests: XCTestCase {}
